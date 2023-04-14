531361b45ebf4720cc2dda218cb575fc1372334f
ekim0n <aki.stromberg@gmail.com>
2023-04-14 14:30:04 +0300

    From layout and index layout fixes

--- js/form.js
+++ js/form.js
@@ -1,3 +1,18 @@
+let allNames = [];
+let allToDo = [];
+
 function submit(){
+
+    const name = document.getElementById("name").value;
+    name.innerHTML = "";
+    const toDo = document.getElementById("todo").value;
+    toDo.innerHTML = "";
+    let result = name + " " + toDo;
+
     
+
+    console.log(result)
+   
+
 }
+
