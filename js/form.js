44f98221aebf9045a38a574a6f3f234028646e81
ekim0n <aki.stromberg@gmail.com>
2023-04-24 12:06:30 +0300

    upgrades for the form

--- js/form.js
+++ js/form.js
@@ -1,13 +1,24 @@
-let allNames = [];
-let allToDo = [];
-
 const date = new Date();
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showData(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+function myFunction() {
+    const list = document.getElementById("list").classList;
+    list.add("myStyle");
+}
 
 
 function submit(){
 
     const name = document.getElementById("name").value;
     const toDo = document.getElementById("todo").value;
+    const check = document.getElementById("important");
     let result =  date + " " + name+ ":" + " " + toDo;
 
     if(name === "" || toDo === "" ){
@@ -17,17 +28,21 @@ function submit(){
         const li = document.createElement("li");
         li.textContent = result;
         toDoList.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        } 
+
+        if (check.checked == true) {
+            document.getElementById("list").classList.add("border");
+          }
         
-    }
+        saveData();
+    
 
-    console.log(result)
-    
-    
-       
-
-    
    
     document.getElementById("name").value = "";
     document.getElementById("todo").value = "";
-}
 
+}
+showData();
