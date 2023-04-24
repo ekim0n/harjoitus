80865a8259b49194c4a18e8ce86599bd5afa371f
ekim0n <aki.stromberg@gmail.com>
2023-04-24 10:01:02 +0300

    javascript for the form

--- js/form.js
+++ js/form.js
@@ -3,19 +3,31 @@ let allToDo = [];
 
 const date = new Date();
 
+
 function submit(){
 
-    document.getElementById("name").value = "";
-    document.getElementById("todo").value = "";
     const name = document.getElementById("name").value;
     const toDo = document.getElementById("todo").value;
-    
-    let result =  date + " " + name + " " + toDo;
+    let result =  date + " " + name+ ":" + " " + toDo;
 
-    
+    if(name === "" || toDo === "" ){
+     return   alert("Fields cannot be empty!");
+    }else{
+        const toDoList = document.getElementById("list");
+        const li = document.createElement("li");
+        li.textContent = result;
+        toDoList.appendChild(li);
+        
+    }
 
     console.log(result)
-   
     
+    
+       
+
+    
+   
+    document.getElementById("name").value = "";
+    document.getElementById("todo").value = "";
 }
 
