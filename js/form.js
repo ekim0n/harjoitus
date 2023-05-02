9abc4613e8c06a1597da736240410b360d32ade9
ekim0n <aki.stromberg@gmail.com>
2023-05-02 15:39:15 +0300

    Updates for the code

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 const date = new Date();
 
+
 function saveData(){
     localStorage.setItem("data", list.innerHTML);
 }
@@ -8,10 +9,7 @@ function showData(){
     list.innerHTML = localStorage.getItem("data");
 }
 
-function myFunction() {
-    const list = document.getElementById("list").classList;
-    list.add("myStyle");
-}
+
 
 
 function submit(){
@@ -19,6 +17,8 @@ function submit(){
     const name = document.getElementById("name").value;
     const toDo = document.getElementById("todo").value;
     const check = document.getElementById("important");
+    const liElement = document.querySelectorAll("li");
+
     let result =  date + " " + name+ ":" + " " + toDo;
 
     if(name === "" || toDo === "" ){
@@ -28,21 +28,26 @@ function submit(){
         const li = document.createElement("li");
         li.textContent = result;
         toDoList.appendChild(li);
-        let span = document.createElement("span")
+        let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+
+        span.addEventListener("click", function() {
+            li.remove();
+            saveData();
+        });
         } 
 
-        if (check.checked == true) {
-            document.getElementById("list").classList.add("border");
-          }
-        
         saveData();
-    
+
+        
 
    
     document.getElementById("name").value = "";
     document.getElementById("todo").value = "";
+    
+    
 
 }
+
 showData();
