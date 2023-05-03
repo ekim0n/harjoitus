55278360d40af8427275d09f11ff46ad9a96f86c
ekim0n <aki.stromberg@gmail.com>
2023-05-03 15:13:53 +0300

    clear all button for the list

--- js/form.js
+++ js/form.js
@@ -1,29 +1,29 @@
 const date = new Date();
 
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", list.innerHTML);
 }
 
-function showData(){
+function showData() {
     list.innerHTML = localStorage.getItem("data");
 }
 
 
 
 
-function submit(){
+function submit() {
 
     const name = document.getElementById("name").value;
     const toDo = document.getElementById("todo").value;
     const check = document.getElementById("important");
     const liElement = document.querySelectorAll("li");
 
-    let result =  date + " " + name+ ":" + " " + toDo;
+    let result = date + " " + name + ":" + " " + toDo;
 
-    if(name === "" || toDo === "" ){
-     return   alert("Fields cannot be empty!");
-    }else{
+    if (name === "" || toDo === "") {
+        return alert("Fields cannot be empty!");
+    } else {
         const toDoList = document.getElementById("list");
         const li = document.createElement("li");
         li.textContent = result;
@@ -37,26 +37,32 @@ function submit(){
         span.innerHTML = "\u00d7";
         li.appendChild(span);
 
-        span.addEventListener("click", function() {
+        span.addEventListener("click", function () {
             li.remove();
             saveData();
-            
+
         });
-        } 
+    }
 
-        saveData();
+    saveData();
 
-        
 
-        
 
-   
+
+
+
     document.getElementById("name").value = "";
     document.getElementById("todo").value = "";
     check.checked = false;
-    
-    
 
+
+
+}
+
+function clearAll() {
+    localStorage.clear();
+    list.innerHTML = "";
+    saveData();
 }
 
 showData();
